93e91b6a7cbe70ec48f5e1e3ccd9935d20a988fb
Katie Pundt <kzpundt@gmail.com>
2021-12-13 09:11:56 -0800

    initial commit
    Co-Authored-by: Albert Lee <leesga8@gmail.com>

---
+++ FriendLetter/Startup.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FriendLetter
+{
+  public class Startup
+  {
+    
+  }
+}
